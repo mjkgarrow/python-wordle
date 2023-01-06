96f265e138cbf1b177da50296e2d2b82e6919406
mjkgarrow <mjkgarrow@gmail.com>
2023-01-06 11:46:03 +1100

    completed game functionality

--- wordle.py
+++ wordle.py
@@ -1,4 +1,3 @@
-import requests
 import curses
 import os
 import random
@@ -8,12 +7,27 @@ from time import sleep
 class Word:
     def __init__(self, word=""):
         self.word = word
+        self.index = 0
 
     def __repr__(self):
         return f"{self.word}"
 
     def __len__(self):
         return len(self.word)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.word):
+            raise StopIteration
+        else:
+            char = self.word[self.index]
+            self.index += 1
+            return char
+
+    def __getitem__(self, index: int):
+        return self.word[index]
 
     def empty(self):
         self.word = ""
@@ -28,38 +42,79 @@ class Word:
     def create_square_string(self):
         return "|" + "|".join(self.word.ljust(5)) + "|"
 
-    def incorrect(self, window):
-        output = self.create_square_string()
-        window.erase()
-        window.addstr(0, 10,
-                      "Not a word!",
-                      curses.color_pair(1))
+    def check_correct(self, wordle_word):
+        return self.word == wordle_word
 
-        window.addstr(2, 10, output,
-                      curses.color_pair(9) + curses.A_UNDERLINE)
+    def incorrect(self, window, round_number, window_sizes):
+        for i in range(len(self.word)):
+            window.addstr(window_sizes[1] + 3 + round_number,
+                          window_sizes[0] + 15 + (i * 2),
+                          self.word[i],
+                          curses.color_pair(1) + curses.A_UNDERLINE)
+            window.addstr(window_sizes[1] + 3 + round_number,
+                          window_sizes[0] + 14 + (i * 2),
+                          "|",
+                          curses.A_UNDERLINE)
+        window.addstr(window_sizes[1] + 3 + round_number,
+                      window_sizes[0] + 24,
+                      "|",
+                      curses.A_UNDERLINE)
         window.refresh()
         sleep(1)
         self.word = ""
 
-    def check_valid(self, wordle_word):
-        if self.word == wordle_word:
-            return True
-        else:
-            return False
+    def accurate_letters(self, wordle_word):
+        letters = []
+        for i in range(len(self.word)):
+            if self.word[i] == wordle_word[i]:
+                letters.append(self.word[i])
+        return letters
 
-    def display_output(self, window, round_number, wordle_word):
-        for i in range(len(repr(self.word))):
-            window.addstr(2 + round_number, 10 + i, "|")
-            if repr(self.word)[i] == wordle_word[i]:
-                window.addstr(2 + round_number, 10 + i + 1, repr(self.word)[i],
-                              curses.color_pair(2))
-            elif repr(self.word)[i] in wordle_word:
-                window.addstr(2 + round_number, 10 + i + 1, repr(self.word)[i],
-                              curses.color_pair(4))
+    def display_output(self, window, round_number, wordle_word, window_sizes):
+        not_found = wordle_word
+
+        for i in range(len(self.word)):
+            if (self.word[i] == wordle_word[i]):
+                not_found = not_found.replace(self.word[i], '', 1)
+                window.addstr(window_sizes[1] + 3 + round_number,
+                              window_sizes[0] + 15 + (i * 2),
+                              self.word[i],
+                              curses.color_pair(2) + curses.A_UNDERLINE)
             else:
-                window.addstr(2 + round_number, 10 + i + 1, repr(self.word)[i],
-                              curses.color_pair(0))
-        window.addstr(2 + round_number, 21, "|")
+                window.addstr(window_sizes[1] + 3 + round_number,
+                              window_sizes[0] + 15 + (i * 2),
+                              self.word[i],
+                              curses.color_pair(0) + curses.A_UNDERLINE)
+
+        for i in range(len(self.word)):
+            if (self.word[i] in not_found) & (self.word[i] != wordle_word[i]):
+                not_found = not_found.replace(self.word[i], '', 1)
+                window.addstr(window_sizes[1] + 3 + round_number,
+                              window_sizes[0] + 15 + (i * 2),
+                              self.word[i],
+                              curses.color_pair(215) + curses.A_UNDERLINE)
+
+            window.addstr(window_sizes[1] + 3 + round_number,
+                          window_sizes[0] + 14 + (i * 2),
+                          "|",
+                          curses.A_UNDERLINE)
+        window.addstr(window_sizes[1] + 3 + round_number,
+                      window_sizes[0] + 24,
+                      "|",
+                      curses.A_UNDERLINE)
+
+
+def get_window_sizes():
+    ''' Find the size of the terminal window and 
+    calculate maximum string width and starting positions'''
+
+    # Get terminal window size [width, height]
+    window_size = [os.get_terminal_size()[0], os.get_terminal_size()[1]]
+
+    # Calculate starting positions based on width and window size
+    text_start_x = (window_size[0]//2) - 30
+    text_start_y = (window_size[1]//2) - 7
+    return [text_start_x, text_start_y]
 
 
 def check_ascii_input(key):
@@ -94,6 +149,160 @@ def check_esc(key):
     return False
 
 
+def print_keyboard(window, accurate_letters,
+                   correct_letters, used_letters, window_sizes):
+    keyboard = [['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+                [' ', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+                [' ', ' ', 'Z', 'X', 'C', 'V', 'B', 'N', 'M']]
+
+    # Print keyboard
+    for i in range(len(keyboard)):
+        for j in range(len(keyboard[i])):
+            if keyboard[i][j].upper() in accurate_letters:
+                window.addstr(window_sizes[1] + 11 + i,
+                              window_sizes[0] + 10 + (j * 2),
+                              keyboard[i][j],
+                              curses.color_pair(2))
+            elif keyboard[i][j].upper() in correct_letters:
+                window.addstr(window_sizes[1] + 11 + i,
+                              window_sizes[0] + 10 + (j * 2),
+                              keyboard[i][j],
+                              curses.color_pair(215))
+            elif keyboard[i][j].upper() in used_letters:
+                window.addstr(window_sizes[1] + 11 + i,
+                              window_sizes[0] + 10 + (j * 2),
+                              " ",
+                              curses.color_pair(0))
+            else:
+                window.addstr(window_sizes[1] + 11 + i,
+                              window_sizes[0] + 10 + (j * 2),
+                              keyboard[i][j],
+                              curses.color_pair(0))
+
+
+def play_game(window, rounds, round, word_list, wordle_word, used_letters, correct_letters, accurate_letters):
+
+    while True:
+        # Get terminal size so game is centred
+        window_sizes = get_window_sizes()  # [x,y]
+
+        window.nodelay(True)
+
+        # Get user input
+        key = window.getch()
+
+        if check_ascii_input(key):
+            rounds[round].add_letter(chr(key))
+            used_letters.append(rounds[round][-1])
+            used_letters = used_letters[: (round * 5) + 5]
+        elif check_backspace(key):
+            rounds[round].remove_letter()
+            if len(rounds[round]) == 0:
+                if len(used_letters) % 5 != 0:
+                    used_letters = used_letters[:-1]
+            else:
+                used_letters = used_letters[:-1]
+        elif key == 9:
+            round = 6
+        elif check_esc(key):
+            quit()
+        elif check_enter_input(key):
+            if len(rounds[round]) == 0:
+                continue
+            if (len(rounds[round]) < 5) or (repr(rounds[round]) not in word_list):
+                used_letters = used_letters[:-len(rounds[round])]
+                rounds[round].incorrect(window, round, window_sizes)
+
+            elif rounds[round].check_correct(wordle_word):
+
+                # Display correct word colouring
+                rounds[round].display_output(window, round,
+                                             wordle_word, window_sizes)
+
+                # Display winning prompt
+                window.nodelay(False)
+                window.move(0, 10)
+                window.clrtoeol()
+                if round == 0:
+                    window.addstr(window_sizes[1] + 3 + round,
+                                  window_sizes[0] + 30,
+                                  f"You got it right in {round + 1} round! Press any button to restart",
+                                  curses.color_pair(2))
+                else:
+                    window.addstr(window_sizes[1] + 3 + round,
+                                  window_sizes[0] + 30,
+                                  f"You got it right in {round + 1} rounds! Press any button to restart",
+                                  curses.color_pair(2))
+
+                # Reset game
+                round = 0
+                used_letters = []
+                correct_letters = []
+                accurate_letters = []
+                for line in rounds:
+                    line.empty()
+                wordle_word = word_list[random.randint(0, len(word_list))]
+
+                window.refresh()
+
+                # Wait for user to press key
+                key = window.getch()
+
+            else:
+                # Update current round
+                round += 1
+
+                # Generate lists of correct letters for keyboard
+                correct_letters = [char for char in
+                                   used_letters if char in wordle_word]
+
+                accurate_letters = [item for sublist in
+                                    [x.accurate_letters(wordle_word) for
+                                     x in rounds] for item in sublist]
+
+        # Clear screen
+        window.erase()
+
+        # Round 6 means game is over
+        if round == 6:
+            window.addstr(window_sizes[1] + 2,
+                          window_sizes[0] + 10,
+                          f"You lost! The word was {wordle_word}",
+                          curses.color_pair(3))
+            window.refresh()
+
+            # Reset game
+            for line in rounds:
+                line.empty()
+            round = 0
+            used_letters = []
+            correct_letters = []
+            accurate_letters = []
+            wordle_word = word_list[random.randint(0, len(word_list))].upper()
+            sleep(2)
+            continue
+        else:
+            window.addstr(window_sizes[1],
+                          window_sizes[0] - 10,
+                          "Press 'esc' to quit, 'enter' to submit guess, 'tab' to see answer",
+                          curses.color_pair(4))
+
+            for line in range(len(rounds)):
+                if line < round:
+                    rounds[line].display_output(window, line,
+                                                wordle_word, window_sizes)
+                else:
+                    window.addstr(window_sizes[1] + 3 + line,
+                                  window_sizes[0] + 14,
+                                  rounds[line].create_square_string(),
+                                  curses.A_UNDERLINE)
+
+        print_keyboard(window, accurate_letters,
+                       correct_letters, used_letters, window_sizes)
+
+        window.refresh()
+
+
 def main(window):
     curses.curs_set(0)
 
@@ -103,66 +312,25 @@ def main(window):
     for i in range(0, curses.COLORS):
         curses.init_pair(i, i, -1)
 
-    # Get 5 letter word
-    # with open("words.txt", "r") as f:
-    #     word_list = f.read().split(",")
-    #     wordle_word = word_list[random.randint(0, len(word_list))]
+    # Get random 5-letter word
+    with open("words.txt", "r") as f:
+        word_list = f.read().upper().splitlines()
+        wordle_word = word_list[random.randint(0, len(word_list))]
 
-    with open("words_short.txt", "r") as f:
-        word_list = f.readlines()
-        wordle_word = word_list[random.randint(0, len(word_list))].strip("\n")
+    # Initialise game variables
+    rounds = [Word(), Word(), Word(), Word(), Word(), Word()]
+    round = 0
+    used_letters = []
+    correct_letters = []
+    accurate_letters = []
 
-    word = Word()
-    rounds = []
-    rounds.append(word)
-
-    while True:
-
-        window.nodelay(True)
-
-        key = window.getch()
-
-        if check_ascii_input(key):
-            word.add_letter(chr(key))
-            rounds[-1] = word
-        elif check_backspace(key):
-            word.remove_letter()
-            rounds[-1] = word
-        elif check_esc(key):
-            quit()
-        elif check_enter_input(key):
-            if len(word) < 5:
-                word.incorrect(window)
-            elif repr(word) not in word_list:
-                word.incorrect(window)
-            elif word.check_valid(wordle_word):
-                rounds.append(word)
-                word.empty()
-                for round in range(len(rounds)):
-                    rounds[round].display_output(window, round, wordle_word)
-
-        window.erase()
-        window.addstr(0, 10,
-                      "Press 'esc' to quit, 'enter' to submit guess",
-                      curses.color_pair(2))
-
-        # for round in range(len(rounds)):
-        #     rounds[round].display_output(window, round, wordle_word)
-
-        # window.addstr(2 + round, 10, rounds[round].create_square_string(),
-        #               curses.A_UNDERLINE)
-        # rounds[round].display_output(window, round_number, wordle_word)
-        # window.addstr(2, 10, word.create_square_string(), curses.A_UNDERLINE)
-        window.refresh()
-
-
-def test():
-    pass
+    # Play game loop
+    play_game(window, rounds, round, word_list, wordle_word,
+              used_letters, correct_letters, accurate_letters)
 
 
 if __name__ == "__main__":
     # Sets delay on escape key to 25 milliseconds
     os.environ.setdefault("ESCDELAY", "25")
 
-    # test()
     curses.wrapper(main)
